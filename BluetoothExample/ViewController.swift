d9992274028a62a8b6bd84f207ba511b96c0f239
nazire <nazire@identitat.com>
2018-06-19 17:51:51 +0200

    Initial Commit

---
+++ BluetoothExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BluetoothExample
+//
+//  Created by Nazire Aslan on 19/06/2018.
+//  Copyright © 2018 Identitat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
